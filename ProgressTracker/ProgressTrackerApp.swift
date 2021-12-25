7f754cc61602f8b58545a4784a521359a7b08f30
Prajjwal009 <sinhaprajjwal009@gmail.com>
2021-12-25 16:54:10 +0530

    Initial Commit

---
+++ ProgressTracker/ProgressTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressTrackerApp.swift
+//  ProgressTracker
+//
+//  Created by Prajjwal on 25/12/21.
+//
+
+import SwiftUI
+
+@main
+struct ProgressTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
